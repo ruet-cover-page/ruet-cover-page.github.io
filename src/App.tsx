7d04da33f44d47425c24e0053b4c3219e0852e9e
Mahmud Nabil <nabilsnigdho@gmail.com>
2024-09-21 17:10:43 +0600

    feat: don' render pdf on mobile while hidden

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useAtomValue } from 'jotai';
 import './App.css';
+import { useEffect, useState } from 'react';
 import { PDFViewer } from './components/PDFViewer';
 import { CoverTemplate } from './components/cover-template';
 import { Editor } from './components/editor/editor';
@@ -7,8 +8,18 @@ import { TopbarLeft, TopbarRight } from './components/topbar';
 import { cn } from './lib/utils';
 import { previewModeAtom } from './store/preview-mode';
 
+const mql = window.matchMedia('(max-width: 1023px)');
+
 const App = () => {
   const previewMode = useAtomValue(previewModeAtom);
+  const [isMobile, setIsMobile] = useState(mql.matches);
+
+  useEffect(() => {
+    const handleChange = (event: { matches: boolean }) =>
+      setIsMobile(event.matches);
+    mql.addEventListener('change', handleChange);
+    return () => mql.removeEventListener('change', handleChange);
+  }, []);
 
   return (
     <main className="fixed inset-0 flex divide-x">
@@ -21,18 +32,20 @@ const App = () => {
         <TopbarLeft />
         <Editor />
       </div>
-      <div
-        className={cn(
-          'flex min-w-0 flex-1 origin-left flex-col divide-y transition-all',
-          previewMode || 'lt-lg:invisible lt-lg:grow-0 lt-lg:scale-x-0',
-          'flex flex-col',
-        )}
-      >
-        <TopbarRight />
-        <PDFViewer className="flex-1">
-          <CoverTemplate />
-        </PDFViewer>
-      </div>
+      {(!isMobile || previewMode) && (
+        <div
+          className={cn(
+            'flex min-w-0 flex-1 origin-left flex-col divide-y transition-all',
+            previewMode || 'lt-lg:invisible lt-lg:grow-0 lt-lg:scale-x-0',
+            'flex flex-col',
+          )}
+        >
+          <TopbarRight />
+          <PDFViewer className="flex-1">
+            <CoverTemplate />
+          </PDFViewer>
+        </div>
+      )}
     </main>
   );
 };
