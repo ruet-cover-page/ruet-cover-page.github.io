7286b3b10f9543e5e2c0f0bd77ede11f22abac65
Mahmud Nabil <nabilsnigdho@gmail.com>
2024-09-30 01:51:44 +0600

    feat: sw

--- src/index.tsx
+++ src/index.tsx
@@ -21,3 +21,16 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.mjs',
   import.meta.url,
 ).toString();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register(`${process.env.ASSET_PREFIX}/sw.js`)
+      .then((registration) => {
+        console.log('SW registered: ', registration);
+      })
+      .catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
