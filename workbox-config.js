ac594c0aca9f63d34ec70727a7d29b5bd410cbb0
Mahmud Nabil <nabilsnigdho@gmail.com>
2024-10-08 16:02:50 +0600

    feat: sw skip wait

--- workbox-config.js
+++ workbox-config.js
@@ -3,4 +3,6 @@ module.exports = {
   globPatterns: ['**/*.{mjs,txt,png,ico,html,webmanifest,jpg,css,ttf,js}'],
   swDest: 'dist/sw.js',
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+  clientsClaim: true, // Takes control of clients immediately after activation
+  skipWaiting: true, // Skip the waiting phase and activate the new service worker right away
 };
