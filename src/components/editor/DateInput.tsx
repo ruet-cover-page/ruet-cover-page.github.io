c6df400d8a41e59c2c62904d38e4206b5a4cc975
Mahmud Nabil <nabilsnigdho@gmail.com>
2024-10-24 03:53:52 +0600

    fix: calender in dark mode

--- src/components/editor/DateInput.tsx
+++ src/components/editor/DateInput.tsx
@@ -1,5 +1,4 @@
-import 'react-date-picker/dist/DatePicker.css';
-import 'react-calendar/dist/Calendar.css';
+import { CalendarIcon, Cross1Icon } from '@radix-ui/react-icons';
 import { type WritableAtom, useAtom } from 'jotai';
 import type { Value } from 'node_modules/react-date-picker/dist/esm/shared/types';
 import DatePicker, { type DatePickerProps } from 'react-date-picker';
@@ -21,6 +20,12 @@ export function DateInput({
         'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:font-medium file:text-foreground file:text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
         classes.dateInput,
       ]}
+      calendarIcon={<CalendarIcon className="hover:text-blue-500" />}
+      clearIcon={<Cross1Icon className="hover:text-blue-500" />}
+      calendarProps={{
+        className: 'bg-background',
+        tileClassName: 'hover:bg-secondary px-4 py-2',
+      }}
       {...props}
     />
   );
