53e7f214fb4dfb61b4c2b5c5bf5d652837f9a4a2
Mahmud Nabil <nabilsnigdho@gmail.com>
2024-10-25 03:34:57 +0600

    feat: SSR

---
+++ post-build.ts
@@ -0,0 +1,46 @@
+import path from 'node:path';
+import { $ } from 'bun';
+import httpServer from 'http-server';
+import puppeteer from 'puppeteer';
+
+const PORT = 3030;
+
+const startServer = () => {
+  const server = httpServer.createServer({
+    root: path.join(__dirname, 'dist'),
+  });
+  return new Promise<typeof server>((resolve) => {
+    server.listen(PORT, 'localhost', () => {
+      console.log(`Server started at http://localhost:${PORT}`);
+      resolve(server);
+    });
+  });
+};
+const server = await startServer();
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+
+await page.goto(`http://localhost:${PORT}`, { waitUntil: 'networkidle0' });
+
+const renderedHTML = await page.$eval('#root', (root) => root.innerHTML);
+
+const indexHTML = await Bun.file(
+  path.join(__dirname, 'dist/index.html'),
+).text();
+
+const updatedHTML = indexHTML
+  .replace(/<div id="root">.*<\/div>/, `<div id="root">${renderedHTML}</div>`)
+  .replaceAll('<script defer>', '<script>');
+
+await Bun.write(path.join(__dirname, 'dist/index.html'), updatedHTML);
+
+await browser.close();
+server.close(() => {
+  console.log('Server stopped.');
+});
+
+await $`bun workbox generateSW workbox-config.cjs`;
+await Bun.write(
+  path.join(__dirname, 'dist/manifest.webmanifest'),
+  Bun.file(path.join(__dirname, 'public/manifest.webmanifest')),
+);
